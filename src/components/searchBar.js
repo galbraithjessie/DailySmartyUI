c933557c737075e700a9de2e5a4a34d27983c0f9
galbraithjessie <galbraithjessie@gmail.com>
2020-03-31 17:40:44 -0600

    Added more routes
    Added exact route to home route, and implemented router HOC into the searchBar to navigate the user to results route.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {Field, reduxForm }  from "redux-form";
+import { withRouter } from "react-router-dom";
 
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
         console.log("trying to handle submit for query", query);
-        
+        this.props.history.push("/results");
     }
 
     renderInput(field) {
@@ -26,6 +27,8 @@ class SearchBar extends Component {
 
 SearchBar = reduxForm({
     form: "searchBar"
-})(SearchBar)
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
