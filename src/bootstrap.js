5fd7f7e8b0231c936ee4c463046ab062eb83a204
galbraithjessie <galbraithjessie@gmail.com>
2020-03-31 15:04:01 -0600

    Set up Component
    Set up the Results Component and added route to the home page

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 
 import Home from "./components/home";
+import Results from "./components/results"
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -18,7 +19,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={Home} />
-          <Route path="/results" component={Home} />
+          <Route path="/results" component={Results} />
         </Switch>
       </BrowserRouter>
     </Provider>,
