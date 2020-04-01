795aba143c7e49997f6d53dda3b1e6197ddcee28
galbraithjessie <galbraithjessie@gmail.com>
2020-04-01 13:56:58 -0600

    Added mapStateToProps
    Added mapStateToProps(state) in the recentPosts.js and rendered the state.posts.recentPosts with li tags

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -8,13 +8,13 @@ const INIT_STATE = {
 }
 
 export default function(state = INIT_STATE, action) {
-    switch (action.typ) {
+    switch (action.type) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload;
             return {
                 ...state,
                  recentPosts
-            };
+            }
     
         default:
             return state;
