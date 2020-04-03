e78e8cdb7f4195b09a984271d55689e9d37c8ea9
galbraithjessie <galbraithjessie@gmail.com>
2020-04-03 09:46:03 -0600

    Set up fetchPostsWithQuery
    Put together fetchPostsWIthQuery action to recieve query posts.

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,13 +7,29 @@ export function fetchRecentPosts() {
         axios
         .get("https://api.dailysmarty.com/posts")
         .then(response => {
-            console.log('axios response', response.data.posts);
+            console.log('axios Recent post response', response.data.posts);
             dispatch({
                 type: SET_RECENT_POSTS,
                 payload: response.data.posts
             })
         }).catch(error => {
             console.log("Recent posts get request error", error);
+        });
+    }
+}
+
+export function fetchPostsWithQuery(query) {
+    return function(dispatch) {
+        axios
+        .get(`https://api.dailysmarty.com/search?q=${query}`)
+        .then(response => {
+            console.log('axios post request response', response.data.posts);
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        }).catch(error => {
+            console.log("Get post request error", error);
         });
     }
 }
