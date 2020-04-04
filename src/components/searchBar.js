2296ab1a8ea34b3ff257e028db44e247e3edfaca
Jessie Galbraith <galbraithjessie@gmail.com>
2020-04-04 13:49:33 -0600

    Added Sass Files
    Added sass files to create mixins, set default layouts, and styled the results page.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {Field, reduxForm }  from "redux-form";
 import { withRouter } from "react-router-dom";
 
+
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
@@ -9,7 +10,7 @@ class SearchBar extends Component {
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
 
     render() {
