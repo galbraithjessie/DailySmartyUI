2af41bfc009f345b4facfd1108c70c79fc63914b
Jessie Galbraith <galbraithjessie@gmail.com>
2020-04-04 14:06:20 -0600

    Styled searchBar
    Styled results component  searchbar

--- src/components/home.js
+++ src/components/home.js
@@ -20,7 +20,7 @@ class Home extends Component {
       <div className='app'>
         <div className="home">
             <Logo />
-            <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+            <SearchBar page="home" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
             <RecentPosts />
         </div>
       </div>
